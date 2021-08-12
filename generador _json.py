a33f0828f12d26fb61e3ec41804deb561aaeb021
AldoCarras <aldosamycarras@gmail.com>
2021-08-11 22:08:07 -0400

    read excel

--- generador _json.py
+++ generador _json.py
@@ -1,2 +1,19 @@
 from openpyxl import Workbook
+from openpyxl import load_workbook
+
+wb = load_workbook("BD_EXCEL.xlsx")
+sheet = wb.get_sheet_by_name('Hoja 1')
+sheet = wb.active
+
+rows = 0
+for max_row, row in enumerate(sheet, 1):
+    if not all(col.value is None for col in row):
+        rows += 1
+
+for i in range(2, rows+1):
+    print(sheet.cell(row=i, column=3).value)
+
+
+
+
 
